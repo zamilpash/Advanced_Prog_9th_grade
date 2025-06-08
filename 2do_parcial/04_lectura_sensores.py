ab6e5828ddb5f3e393106be9da46219d5b024417
Its Zamas <zamilpas17@gmail.com>
2025-06-08 13:09:36 -0600

    Haciendo tarea en domingo :0

--- 2do_parcial/04_lectura_sensores.py
+++ 2do_parcial/04_lectura_sensores.py
@@ -16,16 +16,25 @@ def crear_tabla_lecturas():
     try:
         conn = pymysql.connect(**config)
         cursor = conn.cursor()
-        sql = """CREATE TABLE IF NOT EXISTS Lecturas (
+        sql = """CREATE TABLE IF NOT EXISTS Lecturas_Sensores (
             id INT AUTO_INCREMENT PRIMARY KEY,
             tipo_sensor VARCHAR(30) NOT NULL,
-            valor FLOAT(10) NOT NULL,
+            valor DECIMAL(10) NOT NULL,
+            estado VARCHAR(30) NOT NULL,
             fecha_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )"""
+
+        # Verificación adicional
+        cursor.execute("SHOW COLUMNS FROM Lecturas LIKE 'estado'")
+        if not cursor.fetchone():
+            print("¡Error: La columna 'estado' no se creó!")
+        else:
+            print("Tabla Lecturas creada correctamente con columna 'estado'")
+
         cursor.execute(sql)
         conn.commit()
         conn.close()
-        print("Tabla Lecturas creada")
+        print("Tabla Lecturas_sensores creada")
     except Exception as e:
         print(f"Error al crear tabla: {e}")
 
@@ -37,12 +46,18 @@ def generar_datos_sensor(tipo_sensor):
         return round(random.uniform(50.0, 200.0), 2)
     elif tipo_sensor == "Humedad":
         return round(random.uniform(30.0, 90.0), 2)
-    elif tipo_sensor == "CO2":
+    elif tipo_sensor == "Vibracion":
         return round(random.uniform(400.0, 1500.0), 2)
     elif tipo_sensor == "Luz":
         return round(random.uniform(0.0, 1023.0), 2)
     else:
         return round(random.uniform(0, 100), 2)
+
+# Obtener el estao del sensor
+def estado_sensor():
+    estado = ("Activo", "Inactivo")
+    return random.choice(estado)
+
 
 # Insertar una lectura en la base de datos
 def insertar_dato_sensor(tipo_sensor):
@@ -50,11 +65,12 @@ def insertar_dato_sensor(tipo_sensor):
         conn = pymysql.connect(**config)
         cursor = conn.cursor()
         valor = generar_datos_sensor(tipo_sensor)
-        sql = "INSERT INTO Lecturas (tipo_sensor, valor) VALUES (%s, %s)"
-        cursor.execute(sql, (tipo_sensor, valor))
+        estado_actual = estado_sensor()
+        sql = "INSERT INTO Lecturas_Sensores (tipo_sensor, valor, estado) VALUES (%s, %s, %s)"
+        cursor.execute(sql, (tipo_sensor, valor, estado_actual))
         conn.commit()
         conn.close()
-        print(f"Insertado: {tipo_sensor} = {valor}")
+        print(f"Insertado: {tipo_sensor} = {valor}, Estado: {estado_actual}")
     except Exception as e:
         print(f"Error al insertar dato: {e}")
 
@@ -89,7 +105,8 @@ def limpiar_tabla_lecturas():
 
 # Código principal
 if __name__ == '__main__':
-    crear_tabla_lecturas()  # Crear la tabla una vez
-    limpiar_tabla_lecturas()
-    sensores = ["Temperatura", "Presion", "Humedad", "CO2", "Luz"]
+    crear_tabla_lecturas()  # Crear la tabla
+    limpiar_tabla_lecturas()    # Limpiar tabla para no tener tantos datos
+    sensores = ["Temperatura", "Presion", "Humedad", "Vibracion", "Luz"]
     simular_datos_sensores(sensores)  # Insertar datos simulados
+
